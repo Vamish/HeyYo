abb26ea49e3960046db8b504db0f28aaa6e1bf40
vango <daikaiquan46@gmail.com>
2015-10-27 22:00:13 +0800

    发送短信

--- app/src/main/java/cn/diviniti/heyyo/view/MainActivity.java
+++ app/src/main/java/cn/diviniti/heyyo/view/MainActivity.java
@@ -1,16 +1,29 @@
 package cn.diviniti.heyyo.view;
 
+import android.content.ContentResolver;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import cn.diviniti.heyyo.R;
-import cn.diviniti.heyyo.model.Contact;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,54 +31,81 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListAdapter listAdapter = new SimpleAdapter(this, getContactsData(), R.layout.layout_list_item,
+                new String[]{"name", "phones", "ID"},
+                new int[]{R.id.user_name, R.id.user_phone_number, R.id.user_id});
+        ListView listView = (ListView) findViewById(R.id.contacts);
+        listView.setAdapter(listAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView nameTv = (TextView) view.findViewById(R.id.user_name);
+                final TextView phoneTv = (TextView) view.findViewById(R.id.user_phone_number);
+                new MaterialDialog.Builder(MainActivity.this)
+                        .title("跟" + nameTv.getText() + "打个招呼")
+                        .content("测试用手机号：" + phoneTv.getText().toString().split(";")[0])
+                        .positiveText("确认发送")
+                        .positiveColor(getResources().getColor(R.color.colorPrimaryDark))
+                        .negativeColor(getResources().getColor(R.color.colorPrimaryDark))
+                        .negativeText("还是算了")
+                        .callback(new MaterialDialog.ButtonCallback() {
+                            @Override
+                            public void onPositive(MaterialDialog dialog) {
+                                sendSMS(phoneTv.getText().toString().split(";")[0]);
+//                                Toast.makeText(getApplicationContext(), "发送", Toast.LENGTH_SHORT).show();
+                            }
 
-        List<Contact> test = initContactList();
+                            @Override
+                            public void onNegative(MaterialDialog dialog) {
+                                Toast.makeText(getApplicationContext(), "算了", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+            }
+        });
+
     }
 
-    private List<Contact> initContactList() {
-        List<Contact> contactList = new ArrayList<Contact>();
+    protected List<Map<String, Object>> getContactsData() {
+        List<Map<String, Object>> contactsList = new ArrayList<Map<String, Object>>();
 
-        // 查询联系人数据
-        Cursor cursor = getContentResolver().query(
-                ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        Uri contactUri = ContactsContract.Contacts.CONTENT_URI;
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(contactUri, null, null, null, null);
+
         while (cursor.moveToNext()) {
-            Contact contact = new Contact();
-            // 获取联系人的Id
-            String contactId = cursor.getString(cursor
-                    .getColumnIndex(ContactsContract.Contacts._ID));
-            // 获取联系人的姓名
-            String contactName = cursor.getString(cursor
-                    .getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-            contact.setContactName(contactName);
+            Map<String, Object> contact = new HashMap<String, Object>();
 
-            Cursor phoneCursor = getContentResolver().query(
-                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                    null,
-                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "="
-                            + contactId, null, null);
+            //联系人ID
+            String contactID = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
 
+            //联系人姓名
+            String display_name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+            //联系人号码
+            Uri phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+            String selection = ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactID;
+            Cursor phoneCursor = contentResolver.query(phoneUri, null, selection, null, null);
+
+            String phones = "";
             while (phoneCursor.moveToNext()) {
-                String phoneNumber = phoneCursor
-                        .getString(phoneCursor
-                                .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                contact.setPhoneNumber(phoneNumber);
+                phones += phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DATA));
+                phones += ";";
             }
-
-            if (null != phoneCursor && !phoneCursor.isClosed()) {
-                phoneCursor.close();
+            if (!"".equals(phones.trim())) {
+                contact.put("name", display_name);
+                contact.put("phones", phones);
+                contact.put("ID", contactID);
+                contactsList.add(contact);
             }
-            Log.i("VANGO_DEBUG", contact.getContactName());
-
-            contactList.add(contact);
         }
 
-        if (null != cursor && !cursor.isClosed()) {
-            cursor.close();
-        }
+        return contactsList;
+    }
 
-        Log.i("", contactList.toString());
-
-        return contactList;
+    protected void sendSMS(String phoneNumber) {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, "HeyYO!", null, null);
     }
 }
